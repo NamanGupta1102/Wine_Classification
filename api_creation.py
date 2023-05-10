088314504e6f61c7fb81fe4c7f099c975d3a3f6f
NamanGupta1102 <naman1102@gmail.com>
2023-05-10 20:16:12 +0530

    Update api

--- api_creation.py
+++ api_creation.py
@@ -29,6 +29,7 @@ def predict():
     country_map = {'Armenia': 0, 'Slovakia': 1, 'India': 2, 'Switzerland': 3, 'Luxembourg': 4, 'Czech Republic': 5, 'Serbia': 6, 'Macedonia': 7, 'Cyprus': 8, 'Ukraine': 9, 'Peru': 10, 'Croatia': 11, 'Georgia': 12, 'Morocco': 13, 'Lebanon': 14, 'Brazil': 15, 'Turkey': 16, 'Uruguay': 17, 'Moldova': 18, 'Hungary': 19, 'Mexico': 20, 'Slovenia': 21, 'England': 22, 'Romania': 23, 'Bulgaria': 24, 'Greece': 25, 'Canada': 26, 'Israel': 27, 'South Africa': 28, 'Australia': 29, 'New Zealand': 30, 'Germany': 31, 'Austria': 32, 'Argentina': 33, 'Spain': 34, 'Chile': 35, 'Portugal': 36, 'Italy': 37, 'France': 38, 'US': 39}
     if input_data['country'] not in country_map: 
         return jsonify({'prediction': "Country doesnt exist"})
+    
     input_data['country'] = country_map[input_data['country']]
 
     input_data['review_sentiment'] = TextBlob(input_data['review_description']).sentiment.polarity
